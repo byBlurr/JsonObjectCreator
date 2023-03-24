86c3f406ef40f56edc7de2fa4983a3369fddf795
byBlurr <dbldumont11@gmail.com>
2023-03-24 12:48:38 +0000

    Made a working version for now, will update it as I use it for the Tarkov API

--- JsonObjectCreator/Program.cs
+++ JsonObjectCreator/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿Dictionary<string, string> replace = new Dictionary<string, string>();
+replace.Add("String", "string");
+replace.Add("Boolean", "bool");
+replace.Add("Int", "int");
+replace.Add("Double", "double");
+replace.Add("Float", "float");
+replace.Add("Text", "text");
+replace.Add("ID", "string");
+
+string path = Path.Combine(AppContext.BaseDirectory, "objects.txt");
+string cpath = Path.Combine(AppContext.BaseDirectory, "class.txt");
+
+if (!File.Exists(path)) File.CreateText(path);
+string objList = File.ReadAllText(path);
+string[] objs = objList.Split("\n");
+
+
+string newClass = "";
+foreach (string obj in objs)
+{
+    string clean = obj.TrimEnd();
+    string[] s = clean.Split(": ");
+
+    string type = s[1];
+    if (type.EndsWith('!')) type.Remove(0, type.Length - 1);
+    if (type.StartsWith('[')) type = type.Substring(1).Replace(']', '[') + "]";
+    if (replace.ContainsKey(type)) type = replace[type];
+
+    string l1 = $"[JsonProperty(\"{s[0]}\")]";
+    string l2 = $"public {type} {s[0]} {{ get; private set; }}";
+
+    newClass += l1 + "\n" + l2 + "\n\n";
+}
+
+if (!File.Exists(cpath)) File.CreateText(cpath);
+File.WriteAllText(cpath, newClass);
